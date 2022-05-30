c52782c6ca5f18b2bbe7a1d480af42f92da5c545
mark <markpackham1@gmail.com>
2022-05-30 14:35:27 +0100

    demo use of global variable
    https://youtu.be/BUCiSSyIGGU?t=3944

--- 06_functions.php
+++ 06_functions.php
@@ -1,1 +1,22 @@
 <?php
+
+function registerUser()
+{
+    global $hiThere;
+    $hiThere = 'Hi I am a global variable, no scope can hold me';
+    echo 'User registered';
+}
+
+registerUser();
+
+echo '<br />';
+echo $hiThere;
+
+
+function registerUser2($username)
+{
+    echo "User ${username} has been registered!";
+}
+
+echo '<br />';
+registerUser2("Billy");
