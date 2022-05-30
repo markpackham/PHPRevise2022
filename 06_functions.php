da46a32b2c0808b49cc76bd38f67481a492bba1e
mark <markpackham1@gmail.com>
2022-05-30 14:43:14 +0100

    demo arrow function
    https://youtu.be/BUCiSSyIGGU?t=4188

--- 06_functions.php
+++ 06_functions.php
@@ -4,6 +4,7 @@ function registerUser()
 {
     global $hiThere;
     $hiThere = 'Hi I am a global variable, no scope can hold me';
+    $tapped = 'No one see call me unless I get returned so I am stuck here';
     echo 'User registered';
 }
 
@@ -20,3 +21,29 @@ function registerUser2($username)
 
 echo '<br />';
 registerUser2("Billy");
+
+
+function sum($num1 = 1000, $num2 = 2000)
+{
+    return $num1 + $num2;
+}
+
+echo '<br />';
+$number = sum(1, 2);
+echo $number;
+
+echo '<br />';
+$number2 = sum();
+echo $number2;
+
+
+$subtract = function ($num1, $num2) {
+    return $num1 - $num2;
+};
+echo '<br />';
+echo $subtract(30, 10);
+
+// ugly single line shorthand function
+$multiply = fn ($num1, $num2) => $num1 * $num2;
+echo '<br />';
+echo $multiply(2, 4);
