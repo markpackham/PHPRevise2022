034b8e33d0cd8c86106ad88a36b6c12297dda3dc
mark <markpackham1@gmail.com>
2022-05-30 12:11:42 +0100

    demo empty() use over isset()
    https://youtu.be/BUCiSSyIGGU?t=2908

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -20,3 +20,23 @@ if ($age >= 18) {
 } else {
     echo 'Sorry, you are too young to buy cigarettes.';
 }
+echo "<br />";
+
+$timeHour = date('H');
+if ($timeHour < 12) {
+    echo 'Have a good morning!';
+} elseif ($timeHour < 17) {
+    echo 'Have a good afternoon!';
+} else {
+    echo 'Have a good evening!';
+}
+echo "<br />";
+
+$posts = [];
+// use isset() instead of empty() to get warning if varible doesn't exist
+if (!empty($posts[0])) {
+    echo $posts[0];
+} else {
+    echo 'There are no posts';
+}
+echo "<br />";
