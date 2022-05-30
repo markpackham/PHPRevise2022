c69a6815859cc02e3fb39cdfb8911d7ca0b030cd
mark <markpackham1@gmail.com>
2022-05-30 11:50:58 +0100

    demo basic conditional
    https://youtu.be/BUCiSSyIGGU?t=2659

--- 03_arrays.php
+++ 03_arrays.php
@@ -28,3 +28,32 @@ echo '<br>';
 
 var_dump($hex);
 echo '<br>';
+
+$person1 = [
+    'first_name' => 'John',
+    'last_name' => 'Smith',
+    'email' => 'john@example.com'
+];
+
+$person2 = [
+    'first_name' => 'Jane',
+    'last_name' => 'Doe',
+    'email' => 'jane@example.com'
+];
+
+// echo $person['first_name'];
+$people = [
+    $person1, $person2, [
+        'first_name' => 'Dave',
+        'last_name' => 'Jones',
+        'email' => 'dave@example.com'
+    ]
+];
+
+// get jane@example.com
+echo $people[1]['email'];
+echo '<br>';
+
+// convert to json
+var_dump(json_encode($people));
+echo '<br>';
---
+++ 04_conditionals.php
@@ -0,0 +1,22 @@
+<?php
+
+/* ------------ Operators ----------- */
+
+/*
+  < Less than
+  > Greater than
+  <= Less than or equal to
+  >= Greater than or equal to
+  == Equal to
+  === Identical to
+  != Not equal to
+  !== Not identical to
+*/
+
+$age = 20;
+
+if ($age >= 18) {
+    echo 'You are old enough to buy cigarettes!';
+} else {
+    echo 'Sorry, you are too young to buy cigarettes.';
+}
