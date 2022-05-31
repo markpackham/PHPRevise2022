0910d82e0ff94d702503d80fd599961f5b677f6e
mark <markpackham1@gmail.com>
2022-05-31 09:56:13 +0100

    demo array chunk
    https://youtu.be/BUCiSSyIGGU?t=4538

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -23,3 +23,22 @@ echo '<br>';
 array_unshift($fruits, 'kiwi');
 print_r($fruits);
 echo '<br>';
+
+// pop off from end
+array_pop($fruits);
+// pop off from beginning
+array_shift($fruits);
+// target specific
+unset($fruits[2]);
+
+print_r($fruits);
+echo '<br>';
+
+$fruits = ['apple', 'banana', 'orange', 'mango', 'raspberry', 'kiwi'];
+// split into 2 chunks
+$chunked_array = array_chunk($fruits, 2);
+// Array ( [0] => Array ( [0] => apple [1] => banana ) [1] => Array ( [0] => orange [1] => mango ) [2] => Array ( [0] => raspberry [1] => kiwi ) )
+print_r($chunked_array);
+echo '<br>';
+// array ( 0 => array ( 0 => 'apple', 1 => 'banana', ), 1 => array ( 0 => 'orange', 1 => 'mango', ), 2 => array ( 0 => 'raspberry', 1 => 'kiwi', ), )
+var_export($chunked_array);
