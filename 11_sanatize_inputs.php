c93cad839c95885f2826513431fbc47bc56a5722
mark <markpackham1@gmail.com>
2022-05-31 14:18:29 +0100

    add unsanatized form
    https://youtu.be/BUCiSSyIGGU?t=6114

--- 11_sanatize_inputs.php
+++ 11_sanatize_inputs.php
@@ -1,27 +1,22 @@
 <?php
 
-if (isset($_GET['nameGet']) && isset($_GET['age'])) {
-    echo $_GET['nameGet'];
-    echo '<br>';
-    echo $_GET['age'];
-    echo '<br>';
-}
-
 if (isset($_POST['submit'])) {
     echo '<h3>' . $_POST['username'] . '</h3>';
+    echo '<br>';
+    echo '<h3>' . $_POST['age'] . '</h3>';
     echo '<br>';
 }
 
 ?>
 
-<a href="<?php echo $_SERVER['PHP_SELF']; ?>?nameGet=Jim&age=20">Get Name Jim</a>
-<br>
-<br>
-
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
     <div>
         <label>Name: </label>
         <input type="text" name="username">
+    </div>
+    <div>
+        <label>Age: </label>
+        <input type="text" name="age">
     </div>
     <br>
     <input type="submit" name="submit" value="Submit">
