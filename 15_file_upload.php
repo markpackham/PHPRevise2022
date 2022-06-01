37248cc9794365422b33cf4834613668f087fed8
mark <markpackham1@gmail.com>
2022-06-01 11:57:06 +0100

    check if no file has been submitted
    https://youtu.be/BUCiSSyIGGU?t=7703

--- 15_file_upload.php
+++ 15_file_upload.php
@@ -1,1 +1,31 @@
 <?php
+if (isset($_POST['submit'])) {
+    if (!empty($_FILES['upload']['name'])) {
+    } else {
+        $message = '<p style="color: red;">Please choose a file</p>';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+</head>
+
+<body>
+    <?php echo $message ?? null; ?>
+    <br>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="upload">
+        <input type="submit" value="Submit" name="submit">
+    </form>
+</body>
+
+</html>
