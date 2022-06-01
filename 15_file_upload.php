80e2cbdfa5442e263316ce43aca8d80ff9ad0f16
mark <markpackham1@gmail.com>
2022-06-01 12:15:59 +0100

    check for valid file type
    https://youtu.be/BUCiSSyIGGU?t=8022

--- 15_file_upload.php
+++ 15_file_upload.php
@@ -1,6 +1,23 @@
 <?php
+
+$allowed_ext = array('png', 'jpg', 'jpeg', 'gif');
+
 if (isset($_POST['submit'])) {
     if (!empty($_FILES['upload']['name'])) {
+        // print_r($_FILES);
+        $file_name = $_FILES['upload']['name'];
+        $file_size = $_FILES['upload']['size'];
+        $file_tmp = $_FILES['upload']['tmp_name'];
+        $target_dir = "uploads/${file_name}";
+
+        // get file extension
+        $file_ext = explode('.', $file_name);
+        $file_ext = strtolower(end($file_ext));
+
+        if (in_array($file_ext, $allowed_ext)) {
+        } else {
+            $message = '<p style="color: red;">Invalid file type!</p>';
+        }
     } else {
         $message = '<p style="color: red;">Please choose a file</p>';
     }
@@ -22,7 +39,9 @@ if (isset($_POST['submit'])) {
     <?php echo $message ?? null; ?>
     <br>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-        Select image to upload:
+        <p>
+            Select image to upload ('png', 'jpg', 'jpeg', 'gif'):
+        </p>
         <input type="file" name="upload">
         <input type="submit" value="Submit" name="submit">
     </form>
