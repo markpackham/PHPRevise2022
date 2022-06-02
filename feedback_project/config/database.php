cce843b82bee37206c16a3adfc944dc8f47fb599
mark <markpackham1@gmail.com>
2022-06-02 12:47:56 +0100

    connect to database
    https://youtu.be/BUCiSSyIGGU?t=10434

--- feedback_project/config/database.php
+++ feedback_project/config/database.php
@@ -5,6 +5,7 @@ define('DB_PASS', '');
 define('DB_NAME', 'feedback');
 
 // Create connection
+// Use PDO instead of mysqli if you want to be able to use other types of database
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 // Check connection
