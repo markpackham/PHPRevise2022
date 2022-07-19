bf4d4b52c1b23ba76399a4746d94d07d2ab21247
mark <markpackham1@gmail.com>
2022-07-19 18:38:58 +0100

    mention cache issue

--- CleverTechieTutorials/displayMultiColumnData.php
+++ CleverTechieTutorials/displayMultiColumnData.php
@@ -5,9 +5,10 @@
 </head>
 
 <body>
+    <p>Apache server must be stopped and restarted to notice change in max_columns numbers.</p>
     <?php
 
-    $max_columns = 4;
+    $max_columns = 8;
     $data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     ?>
 
