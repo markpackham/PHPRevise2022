3150a4af80e9b3c0a8a860509e83d89379031971
mark <markpackham1@gmail.com>
2022-07-20 17:15:07 +0100

    close directory reading
    https://youtu.be/QAPZUNHQoNo?t=680

--- CleverTechieTutorials/readListLocalFiles.php
+++ CleverTechieTutorials/readListLocalFiles.php
@@ -13,13 +13,16 @@ $local_dir = 'C:\xampp\htdocs\PHPRevise2022\CleverTechieTutorials';
 // scandir() solution
 
 // $files = scandir($local_dir);
-// remove unwanted values to only show files and not parent directory paths eg "./"
+// remove unwanted values to only show files and not parent directory eg "./"
 // $files = array_diff($files, array('.', '..'));
 // $files = array_values($files);
 // pre_r($files);
 
 // $files = array_values(array_diff(scandir($local_dir), array('.', '..')));
 // pre_r($files);
+
+echo "scandir() solution";
+echo "<br/>";
 
 function clean_scandir($dir)
 {
@@ -28,6 +31,17 @@ function clean_scandir($dir)
 
 pre_r(clean_scandir($local_dir));
 
-
-
+echo "<br/>";
+echo "<br/>";
+echo "opendir() and readir() solution.";
+echo "<br/>";
+echo "<br/>";
 // opendir() and readir()
+if ($handle = opendir($local_dir)) {
+    while (false !== ($file = readdir($handle))) {
+        if ($file != '.' && $file != '..') {
+            $files[] = $file;
+        }
+    }
+    closedir($handle);
+}
