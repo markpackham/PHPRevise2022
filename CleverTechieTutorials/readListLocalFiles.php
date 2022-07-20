39b0cb7a5ce9583a5561a153ce5538e42fd8b571
mark <markpackham1@gmail.com>
2022-07-20 16:46:22 +0100

    finish off refactoring scan dir
    https://youtu.be/QAPZUNHQoNo?t=474

--- CleverTechieTutorials/readListLocalFiles.php
+++ CleverTechieTutorials/readListLocalFiles.php
@@ -10,15 +10,24 @@ function pre_r($array)
 $local_dir = 'C:\xampp\htdocs\PHPRevise2022\CleverTechieTutorials';
 
 
-// opendir() and readir()
-
-
 // scandir() solution
 
 // $files = scandir($local_dir);
+// remove unwanted values to only show files and not parent directory paths eg "./"
 // $files = array_diff($files, array('.', '..'));
 // $files = array_values($files);
 // pre_r($files);
 
-$files = array_values(array_diff(scandir($local_dir), array('.', '..')));
-pre_r($files);
+// $files = array_values(array_diff(scandir($local_dir), array('.', '..')));
+// pre_r($files);
+
+function clean_scandir($dir)
+{
+    return array_values(array_diff(scandir($dir), array('.', '..')));
+}
+
+pre_r(clean_scandir($local_dir));
+
+
+
+// opendir() and readir()
