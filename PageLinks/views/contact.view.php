8d3606f7f30fb5110a85b30819abbfa9cee452a8
mark <markpackham1@gmail.com>
2022-10-14 19:16:24 +0100

    add banner
    https://youtu.be/dda0u9I2mog?t=391

--- PageLinks/views/contact.view.php
+++ PageLinks/views/contact.view.php
@@ -1,15 +1,10 @@
 <?php require('partials/head.php') ?>
 <?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
 
-<header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
-    </div>
-</header>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p>Welcome to the Contact page</p>
-        <!-- /End replace -->
     </div>
 </main>
 
