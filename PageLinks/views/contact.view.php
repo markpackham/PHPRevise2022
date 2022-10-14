506ad78bb1f83b67949504a61518f7ff47ed1162
mark <markpackham1@gmail.com>
2022-10-14 18:55:18 +0100

    demo nav partial
    https://youtu.be/dda0u9I2mog?t=169

---
+++ PageLinks/views/contact.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Links</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<!-- Taken from https://tailwindui.com/components/application-ui/application-shells/stacked -->
+
+<body class="h-full">
+    <div class="min-h-full">
+
+        <?php require('partials/nav.php') ?>
+
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                <p>Welcome to the Contact page</p>
+                <!-- /End replace -->
+            </div>
+        </main>
+    </div>
+
+</body>
+
+</html>
