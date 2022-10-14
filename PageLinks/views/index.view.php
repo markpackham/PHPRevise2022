e2d66c943c9bc8be65dd29109d7ea3a7e7e4ea80
mark <markpackham1@gmail.com>
2022-10-14 19:00:41 +0100

    add head and foot partials
    https://youtu.be/dda0u9I2mog?t=273

--- PageLinks/views/index.view.php
+++ PageLinks/views/index.view.php
@@ -1,33 +1,16 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Links</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<!-- Taken from https://tailwindui.com/components/application-ui/application-shells/stacked -->
-
-<body class="h-full">
-    <div class="min-h-full">
-
-        <?php require('partials/nav.php') ?>
-
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <p>Welcome to the homepage</p>
-                <!-- /End replace -->
-            </div>
-        </main>
+<header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
     </div>
+</header>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Welcome to the homepage</p>
+        <!-- /End replace -->
+    </div>
+</main>
 
-</body>
-
-</html>
+<?php require('partials/foot.php') ?>
